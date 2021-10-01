59dbb4e620d074051b286d1f9be9407bcf0c7280
raihan-off <raihan.official230@gmail.com>
2021-10-01 10:53:43 +0700

    first commit

---
+++ DataDiri.java
@@ -0,0 +1,12 @@
+public class DataDiri{
+
+	public static void main(String[] args){
+		String Nama = "Muhammad Raihan";
+		long NIM = 6706213033l;
+		double IPK = 3.9;
+	
+		System.out.println("Nama : " + Nama);
+		System.out.println("NIM : " + NIM);
+		System.out.println("IPK: " + IPK);
+	}
+}
